9295cc39be5807a3eeb4dc2d9768d3b6c76f577a
Evandro oliveira <vandoaparecido@hotmail.com>
2021-07-20 09:27:54 -0300

    :alarm_clock: Pegando horas minutos e segundos, atualizando relógio

--- relogio/script.js
+++ relogio/script.js
@@ -2,3 +2,12 @@ let digitalElement = document.querySelector(".digital");
 let sElement = document.querySelector(".p_s");
 let mElement = document.querySelector(".p_m");
 let hElement = document.querySelector(".p_h");
+
+function updateClock() {
+  let now = new Date();
+  let hour = now.getHours();
+  let minute = now.getMinutes();
+  let second = now.getSeconds();
+}
+
+setInterval(updateClock, 1000);

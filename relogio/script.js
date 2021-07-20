0b8fedb33f001656db1c6d4d3a105347702735aa
Evandro oliveira <vandoaparecido@hotmail.com>
2021-07-20 09:22:29 -0300

    :alarm_clock: Selecionando elementos

---
+++ relogio/script.js
@@ -0,0 +1,4 @@
+let digitalElement = document.querySelector(".digital");
+let sElement = document.querySelector(".p_s");
+let mElement = document.querySelector(".p_m");
+let hElement = document.querySelector(".p_h");

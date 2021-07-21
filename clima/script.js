12cc46a75286d46962fe82cd92863b6efdfc455c
Evandro oliveira <vandoaparecido@hotmail.com>
2021-07-21 08:41:31 -0300

    :sun_behind_rain_cloud: Capturando valor do imput

--- clima/script.js
+++ clima/script.js
@@ -1,3 +1,6 @@
 document.querySelector('.busca').addEventListener('submit',(event)=>{
     event.preventDefault();
+
+    let input = document.querySelector('#searchInput').value;
+    console.log(input);
 }

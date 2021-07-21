a9fa02413feb90785ff0182f072338cc9df270cf
Evandro oliveira <vandoaparecido@hotmail.com>
2021-07-21 09:03:48 -0300

    :sun_behind_rain_cloud: Utilizando a função fetch

--- clima/script.js
+++ clima/script.js
@@ -1,13 +1,18 @@
-document.querySelector(".busca").addEventListener("submit", (event) => {
+document.querySelector(".busca").addEventListener("submit", async (event) => {
   event.preventDefault();
 
   let input = document.querySelector("#searchInput").value;
 
   if (input !== "") {
     showWarning("Carregando...");
+
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(
       input
     )}&appid=23732ccd8c5274148b542da31e2b146b&units=metric&lang=pt_br`;
+
+    let results = await fetch(url);
+    let json = await results.json();
+    console.log(json);
   } else {
   }
 });

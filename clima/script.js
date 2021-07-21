143db2a0ca76bbc464b259c6465315dd02cb0e2d
Evandro oliveira <vandoaparecido@hotmail.com>
2021-07-21 08:56:04 -0300

    :sun_behind_rain_cloud: Validando e criando url

--- clima/script.js
+++ clima/script.js
@@ -1,6 +1,17 @@
-document.querySelector('.busca').addEventListener('submit',(event)=>{
-    event.preventDefault();
+document.querySelector(".busca").addEventListener("submit", (event) => {
+  event.preventDefault();
 
-    let input = document.querySelector('#searchInput').value;
-    console.log(input);
+  let input = document.querySelector("#searchInput").value;
+
+  if (input !== "") {
+    showWarning("Carregando...");
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(
+      input
+    )}&appid=23732ccd8c5274148b542da31e2b146b&units=metric&lang=pt_br`;
+  } else {
+  }
+});
+
+function showWarning(msg) {
+  document.querySelector(".aviso").innerHTML = msg;
 }

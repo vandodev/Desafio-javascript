e8258bfe8df1d40896bee1f251541e7ff5cea58b
Evandro oliveira <vandoaparecido@hotmail.com>
2021-07-22 11:52:31 -0300

    :video_game: Preenchendo e selecionando jogador ao clickar em resetar

--- jogodavelha/script.js
+++ jogodavelha/script.js
@@ -3,11 +3,11 @@ let square = {
   a1: "",
   a2: "",
   a3: "",
-  b1: "",
+  b1: "x",
   b2: "",
   b3: "",
   c1: "",
-  c2: "",
+  c2: "o",
   c3: "",
 };
 let vez = "";
@@ -16,9 +16,6 @@ let playng = false;
 
 //Events
 document.querySelector(".reset").addEventListener("click", reset);
-document.querySelectorAll(".item").forEach((item) => {
-  item.addEventListener("click", itemClick);
-});
 
 //Functions
 
@@ -28,9 +25,25 @@ function reset() {
   let random = Math.floor(Math.random() * 2);
   player = random === 0 ? "X" : "O";
 
-  for (let i in square) {
-    square[i] = "";
-  }
+  //   for (let i in square) {
+  //     square[i] = "";
+  //   }
 
   playing = true;
+  renderSquare();
+  renderInfo();
+}
+
+//Função responsável por mostrar os valores no tabuleiro.
+function renderSquare() {
+  for (let i in square) {
+    let item = document.querySelector(`div[data-item=${i}]`);
+    item.innerHTML = square[i];
+  }
+}
+
+//Função responsável por exibir a informação de turno do jogador e mensagens.
+function renderInfo() {
+  document.querySelector(".vez").innerHTML = player;
+  document.querySelector(".resultado").innerHTML = warning;
 }

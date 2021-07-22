e6231715c7d1c1b386a0cba41fa5087f05b0b678
Evandro oliveira <vandoaparecido@hotmail.com>
2021-07-22 09:53:02 -0300

    :video_game: Dados iniciais

---
+++ jogodavelha/script.js
@@ -0,0 +1,18 @@
+//Initial data
+let square = {
+  a1: "",
+  a2: "",
+  a3: "",
+  b1: "",
+  b2: "",
+  b3: "",
+  c1: "",
+  c2: "",
+  c3: "",
+};
+let vez = "";
+let warning = "";
+let playng = false;
+
+//Events
+//Functions

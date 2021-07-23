3bb226505299aa6e6d0556ced04381f3b7a3fc3d
Evandro oliveira <vandoaparecido@hotmail.com>
2021-07-23 09:58:01 -0300

    :paintbrush: Detectando ação de clicar soltar e moder o mouse

--- canvas/script.js
+++ canvas/script.js
@@ -1,9 +1,32 @@
+//iNITIAL DATA
 let currentColor = "black";
+
+let screen = document.querySelector("#tela");
+let ctx = screen.getContext("2d");
+
+//eVENTS
 document.querySelectorAll(".colorArea .color").forEach((item) => {
   item.addEventListener("click", colorClickEvent);
 });
 
+screen.addEventListener("mousedown", mouseDownEvent);
+screen.addEventListener("mousemove", mouseMoveEvent);
+screen.addEventListener("mouseup", mouseUpEvent);
+
+//fUNCTIONS
 function colorClickEvent(e) {
   let color = e.target.getAttribute("data-color");
   console.log("Cor clicada", color);
+}
+
+function mouseDownEvent() {
+  console.log("Clicou");
+}
+
+function mouseMoveEvent() {
+  console.log("Moveu");
+}
+
+function mouseUpEvent() {
+  console.log("Soltou");
 }

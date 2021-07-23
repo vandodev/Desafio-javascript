74bf3ac02d222afe88b2fd084ab813c0221ea332
Evandro oliveira <vandoaparecido@hotmail.com>
2021-07-23 10:29:43 -0300

    :paintbrush: Criando a função de desenhar

--- canvas/script.js
+++ canvas/script.js
@@ -1,6 +1,8 @@
 //iNITIAL DATA
 let currentColor = "black";
 let canDraw = false;
+let mouseX = 0;
+let mouseY = 0;
 
 let screen = document.querySelector("#tela");
 let ctx = screen.getContext("2d");
@@ -20,20 +22,35 @@ function colorClickEvent(e) {
   console.log("Cor clicada", color);
 }
 
-function mouseDownEvent() {
+function mouseDownEvent(e) {
   canDraw = true;
+  mouseX = e.pageX - screen.offsetLeft;
+  mouseY = e.pageY - screen.offsetTop;
 }
 
 function mouseMoveEvent(e) {
   if (canDraw) {
-    //Mostra posição x e y
-    console.log(e.pageX, e.pageY);
-    //Corrigindo posição
-    mouseX = e.pageX - screen.offsetLeft;
-    mouseY = e.pageY - screen.offsetTop;
+    draw(e.pageX, e.pageY);
   }
 }
 
 function mouseUpEvent() {
   canDraw = false;
+}
+
+function draw(x, y) {
+  let pointX = x - screen.offsetLeft;
+  let pointY = y - screen.offsetTop;
+
+  ctx.beginPath();
+  ctx.lineWidth = 5;
+  ctx.lineJoin = "round";
+  ctx.moveTo(mouseX, mouseY);
+  ctx.lineTo(pointX, pointY);
+  ctx.closePath();
+  ctx.strokeStyle = currentColor;
+  ctx.stroke();
+
+  mouseX = pointX;
+  mouseY = pointY;
 }

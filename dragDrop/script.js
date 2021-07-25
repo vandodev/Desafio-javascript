5affcb958c6329770068cd48cfe75a0dcba21536
Evandro oliveira <vandoaparecido@hotmail.com>
2021-07-25 09:41:18 -0300

    :game_die: identificando clice

--- dragDrop/script.js
+++ dragDrop/script.js
@@ -1,3 +1,4 @@
-document.querySelector(".neutralArea").addEventListener("click", () => {
-  console.log("Clicou");
+document.querySelector(".neutralArea").addEventListener("click", (e) => {
+  console.log("TARGET", e.target);
+  console.log("CURRENT_TARGET", e.currentTarget);
 });
